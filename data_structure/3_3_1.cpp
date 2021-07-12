ee808f86684342943a26afe19c3800b84ff1a3fd
“zjuadurey” <1721966349@qq.com>
2021-07-12 21:31:23 +0800

    3.0

--- data_structure/3_3_1.cpp
+++ data_structure/3_3_1.cpp
@@ -27,6 +27,7 @@ public:
     bool Insert(DataType x, int i);
     bool Delete(int i);
     void Print();
+    DataType Get(int i);
 };
 
 void Link::Initiate()
@@ -100,9 +101,136 @@ void Link::TailCreateWithHead(int n)
         s = new Item();
         cin>>s->data;
         r->next=s;//assign s to pointer field of r
-        r=s;
+        r=s;  // the end of the list is the end of Item s
     }
     r->next = NULL;
     head = p;
 }
 
+int Link::Length()
+{
+    int count = 0;
+    Item *p;
+    p= head->next;
+    while(p)
+    {
+        count++;
+        p = p->next;
+    }
+    return count;
+}
+
+Item * Link::Locatei(int i)
+{
+    Item *p;
+    if(i==0) return head;
+    p = head->next;
+    int count=1;
+
+    while(count<i && p!=NULL)
+    {
+        p = p->next;
+        count++;
+    }
+    if(count==i) return p;
+    else{
+        cout<<"position i is not correct! "<<endl;
+        return NULL;
+    }
+}
+
+Item * Link::Locatex(DataType x)
+{
+    Item *p = head->next;
+    while(p && p->data!=x) p = p->next;
+    if(p) return p;
+    else{
+        cout << x << " is not exist! "<<endl;
+        return NULL;
+    }
+}
+
+DataType Link::Get(int i)
+{
+    Item *p = head->next;
+    int count = 1;
+    while(p && count<i)
+    {
+        p = p->next;
+        count++;
+    }
+    if(!p||count>i)
+    {
+        cout<<"position is not correct! " <<endl;
+        return 0;
+    }
+    else return p->data;
+}
+
+bool Link::Insert(DataType x, int i)
+{
+    Item *p, *s;
+    p = Locatei(i-1);
+    if(!p) return false;
+    s = new Item();
+    s->data = x;
+    s->next = p->next;
+    p->next = s;
+    return true;
+}
+
+bool Link::Delete(int i)
+{
+    Item *p = Locatei(i-1);
+    if(!p)
+    {
+        cout << "position is not correct! "<<endl;
+        return false;
+    }
+    Item  *q = p->next;
+    if(q)
+    {
+        p->next = q->next;
+        delete q;
+        return true;
+    }
+    else{
+        cout << "position is not correct! "<<endl;
+        return false;
+    }
+}
+
+void Link::Print()
+{
+    Item *p = head->next;
+    while(p)
+    {
+        cout << p->data<< " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+void Link::DeleteAll()
+{
+    Item *p = head, *q;
+    while(p)
+    {
+        q = p->next;
+        delete p;
+        p = q;
+    }
+    head = NULL;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    Link a;
+    a.Initiate();
+    a.TailCreateWithHead(n);
+    a.Delete(7);
+    a.Print();
+    return 0;
+}
